5cfff810b155a07af1afe8c748f4ea1664e327c8
Louis Kenyon <louiskenyon98@gmail.com>
2020-10-15 18:50:13 +0100

    Create AppConfig file and declare RestTemplate Bean

---
+++ src/main/java/co/uk/kenyoncomputing/mixfix/AppConfig.java
@@ -0,0 +1,13 @@
+package co.uk.kenyoncomputing.mixfix;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
