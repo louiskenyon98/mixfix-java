54c8dded5628f4867cee2276d105ec83064a9349
Louis Kenyon <louiskenyon98@gmail.com>
2020-10-23 17:26:09 +0100

    Allow user to obtain authorisation and set/verify state cookie correctly

--- src/main/java/co/uk/kenyoncomputing/mixfix/controllers/AuthController.java
+++ src/main/java/co/uk/kenyoncomputing/mixfix/controllers/AuthController.java
@@ -3,26 +3,42 @@ package co.uk.kenyoncomputing.mixfix.controllers;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
-@RequestMapping("auth")
 public class AuthController {
 
     @GetMapping("spotifyLogin")
-    public ModelAndView spotifyLogin(ModelMap modelMap) {
+    public ModelAndView spotifyLogin(ModelMap modelMap, HttpServletResponse response) {
         String state = RandomStringUtils.randomAlphabetic(16);
+        String stateKey = "spotify_auth_state";
 
+        Cookie stateCookie = new Cookie(stateKey, state);
+
+// ModelMap code
+        response.addCookie(stateCookie);
         modelMap.addAttribute("response_type", "code");
         modelMap.addAttribute("client_id", "9f8e38fa4a4b4121a8108c36674f4a67");
         modelMap.addAttribute("scope", "user-read-private user-read-email");
         modelMap.addAttribute("redirect_uri", "http://localhost:8080/callback");
-        modelMap.addAttribute("state", RandomStringUtils.randomAlphabetic(16));
+        modelMap.addAttribute("state", state);
         return new ModelAndView("redirect:https://accounts.spotify.com/authorize?", modelMap);
+    }
+
+    //use HttpServletRequest request to inspect the incoming request
+    @GetMapping("callback")
+    public ModelAndView callback(@RequestParam("code") String code, @RequestParam("state") String state, @CookieValue(value = "spotify_auth_state", defaultValue = "") String storedState, ModelMap modelMap) {
+        if (state == null || !state.equals(storedState) || storedState.equals("")) {
+            modelMap.addAttribute("error", "state_mismatch");
+            return new ModelAndView("redirect:jimbo", modelMap);
+        } else {
+            return new ModelAndView("redirect:authSuccess");
+        }
     }
 
     @GetMapping("obtainToken")
@@ -32,5 +48,15 @@ public class AuthController {
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
         System.out.println(responseEntity);
         return responseEntity;
+    }
+
+    @GetMapping("authSuccess")
+    public String authSuccess() {
+        return "Success";
+    }
+
+    @GetMapping("jimbo")
+    public String hashRouter() {
+        return "hash route";
     }
 }
