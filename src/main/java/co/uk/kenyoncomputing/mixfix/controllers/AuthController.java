f5f042195b6b640a0f144a90c228f92681fb5495
Louis Kenyon <louiskenyon98@gmail.com>
2020-10-31 16:31:13 +0000

    Get authorisation data from Spotify and store as variable

--- src/main/java/co/uk/kenyoncomputing/mixfix/controllers/AuthController.java
+++ src/main/java/co/uk/kenyoncomputing/mixfix/controllers/AuthController.java
@@ -1,8 +1,10 @@
 package co.uk.kenyoncomputing.mixfix.controllers;
 
 import org.apache.commons.lang3.RandomStringUtils;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,7 +22,6 @@ public class AuthController {
 
         Cookie stateCookie = new Cookie(stateKey, state);
 
-// ModelMap code
         response.addCookie(stateCookie);
         modelMap.addAttribute("response_type", "code");
         modelMap.addAttribute("client_id", "9f8e38fa4a4b4121a8108c36674f4a67");
@@ -32,11 +33,32 @@ public class AuthController {
 
     //use HttpServletRequest request to inspect the incoming request
     @GetMapping("callback")
-    public ModelAndView callback(@RequestParam("code") String code, @RequestParam("state") String state, @CookieValue(value = "spotify_auth_state", defaultValue = "") String storedState, ModelMap modelMap) {
+    public ModelAndView callback(@RequestParam("code") String authCode, @RequestParam("state") String state, @CookieValue(value = "spotify_auth_state", defaultValue = "") String storedState, ModelMap modelMap) {
+        //if state cookie doesn't match returned state value, or state cookie is blank
         if (state == null || !state.equals(storedState) || storedState.equals("")) {
             modelMap.addAttribute("error", "state_mismatch");
             return new ModelAndView("redirect:jimbo", modelMap);
         } else {
+            System.out.println("authCode: " + authCode);
+
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+            MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+
+            map.add("grant_type", "authorization_code");
+            map.add("code", authCode);
+            map.add("redirect_uri", "http://localhost:8080/callback");
+            map.add("client_id", "9f8e38fa4a4b4121a8108c36674f4a67");
+            map.add("client_secret", "3d5314d4ad274be38c36aeb8bfacdc80");
+
+            String url = "https://accounts.spotify.com/api/token";
+
+            HttpEntity<MultiValueMap<String, String>> postRequest = new HttpEntity<>(map, headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, postRequest, String.class);
+
             return new ModelAndView("redirect:authSuccess");
         }
     }
