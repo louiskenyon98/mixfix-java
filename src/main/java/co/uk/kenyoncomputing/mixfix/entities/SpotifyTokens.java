f23542e266b1a231214f0117320dd08188918eae
Louis Kenyon <louiskenyon98@gmail.com>
2021-01-03 03:19:37 +0000

    Add SpotifyTokens entity

---
+++ src/main/java/co/uk/kenyoncomputing/mixfix/entities/SpotifyTokens.java
@@ -0,0 +1,89 @@
+package co.uk.kenyoncomputing.mixfix.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class SpotifyTokens {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    private String scope;
+
+    @JsonProperty("expires_in")
+    private int expiry;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    public SpotifyTokens() {
+
+    }
+
+    public SpotifyTokens(String accessToken, String tokenType, String scope, int expiry, String refreshToken) {
+        this.accessToken = accessToken;
+        this.tokenType = tokenType;
+        this.scope = scope;
+        this.expiry = expiry;
+        this.refreshToken = refreshToken;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public int getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(int expiry) {
+        this.expiry = expiry;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
